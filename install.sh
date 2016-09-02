99e0e0caee1f90d6b50ea2b2dc91ef6d2a40a665
Marat Khusnetdinov <marat@khusnetdinov.ru>
2016-09-02 23:16:12 +0300

    packages

--- install.sh
+++ install.sh
@@ -4,13 +4,23 @@ OS=`uname`
 
 darwin_installation() {
   echo "Start installation for MacOS"
-
   insttalation
 }
 
 linux_installation() {
-  echo "Start installation for Linux"
 
+  updates_system() {
+    sudo apt-get update
+  }
+
+  install_packages() {
+    sudo apt-get install curl
+    sudo apt-get install build-essential libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip
+    sudo apt-get install git git-core git-flow
+  }
+
+  echo "Start installation for Linux"
+  updates_system
   installation
 }
 
