f65ef602adb4c038599a39c14638ef2de39d858b
capitan <capitan@capitans-Mac.local>
2016-09-07 12:11:42 +0300

    Updates for docs

--- install.sh
+++ install.sh
@@ -12,24 +12,9 @@ darwin_installation() {
 }
 
 linux_installation() {
-
-  updates_system() {
-    sudo apt-get update
-  }
-
-  install_packages() {
-    sudo apt-get install curl
-    sudo apt-get install build-essential libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip
-    sudo apt-get install git-core git-flow
-  }
-
   echo ""
   echo "Start installation for Linux"
   echo ""
-
-  updates_system
-  install_packages
-  installation
 }
 
 installation() {
